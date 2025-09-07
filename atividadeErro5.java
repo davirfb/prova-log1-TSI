ada82f392d23ae2a6adc8ad31d5a7297fda6c705
Davi Rocha <163659635+davirfb@users.noreply.github.com>
2025-09-07 18:01:02 -0300

    atividade 5 e 6 erros

--- atividadeErro5.java
+++ atividadeErro5.java
@@ -2,6 +2,27 @@ package Prova1;
 /*Ler a hora de início e a hora de fim de um jogo de Xadrez (considere apenas horas inteiras, sem
 os minutos) e calcule a duração do jogo em horas, sabendo-se que o tempo máximo de duração do
 jogo é de 24 horas e que o jogo pode iniciar em um dia e terminar no dia seguinte. */
-public class atividadeErro5 {
 
+import java.util.Scanner;
+
+public class atividadeErro5 {
+    public static void main(String[] args) {
+    Scanner scanner =new Scanner(System.in);
+    System.out.println("Informe a hora(sem os minutos) que começou o jogo de xadrez:");
+    int horaIni =  scanner.nextInt();
+    System.out.println("Infomre a hora(sem os minutos) que acabou o jogo de xadrez:");
+    int horaFim =  scanner.nextInt();   
+    int duracao;
+    
+    if (horaFim > horaIni) {
+            duracao = horaFim - horaIni;
+        } else if (horaFim < horaIni) {
+            duracao = 24 - (horaIni - horaFim);
+        } else { 
+            duracao = 24;
+        }
+     System.out.println("A duração do jogo foi de " + duracao + " hora(s).");
+
+    scanner.close();
+     }
 }
--- atividadeErro6.java
+++ atividadeErro6.java
@@ -1,8 +1,28 @@
 package Prova1;
+
+import java.util.Scanner;
+
 /*O custo de um carro novo ao consumidor é a soma do custo de fábrica coma porcentagem do
 distribuidor e dos impostos (aplicados ao custo de fábrica). Supondo que o percentual do
 distribuidor seja de 28% e os impostos de 45%, escrever um algoritmo para ler o custo de fábrica
 de um carro, calcular e escrever o custo final ao consumidor. */
 public class atividadeErro6 {
+public static void main(String[] args) {
+    Scanner scanner =new Scanner(System.in);
 
+    System.out.println("-Calculando o valor do carro-\n");
+    System.out.println("Informe o valor do custo de fábrica:");
+    Double valorFabrica = scanner.nextDouble();
+
+    //calculo das % do Distribuidor e Imposto
+    Double parteDistribuidor = 0.28 * valorFabrica;
+    Double parteImposto = 0.45 * valorFabrica;
+
+    double valorFinal = parteDistribuidor + parteImposto + valorFabrica;
+
+    String resultado = String.format("%.2f", valorFinal); 
+    System.out.println(resultado);
+
+
+}
 }
